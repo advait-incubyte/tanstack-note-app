26a007f346b6b9d3847c4c1fbb8bf688dd9ff352
Advait Maindalkar <advait@incubyte.co>
2025-06-20 07:05:24 +0530

    fix: added main css import for styles to work

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+import './index.css'
+
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
