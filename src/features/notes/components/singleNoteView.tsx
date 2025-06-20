2c20adc4157066a2e1a310a9c2721f52038ce07d
Advait Maindalkar <advait@incubyte.co>
2025-06-20 07:56:03 +0530

    feat: added route and page component for single note view

---
+++ src/features/notes/components/singleNoteView.tsx
@@ -0,0 +1,9 @@
+const SingleNoteView: React.FC = () => {
+    return (
+        <div>
+            <h1>Note Details</h1>
+        </div>
+    )
+}
+
+export default SingleNoteView
---
+++ src/routes/$noteId.tsx
@@ -0,0 +1,10 @@
+import { createFileRoute } from '@tanstack/react-router'
+import SingleNoteView from '@/features/notes/components/singleNoteView'
+
+export const Route = createFileRoute('/$noteId')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return <SingleNoteView />
+}
